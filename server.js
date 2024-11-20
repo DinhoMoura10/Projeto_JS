0cfc546871479fbf247bff00153c49c043af9122
DinhoMoura10 <andreimoura1404@gmail.com>
2024-11-20 14:48:03 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import routes from "./src/routes/postsRoutes.js";
+routes
+const app = express();
+routes(app);
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
+});
+
+
+
+
